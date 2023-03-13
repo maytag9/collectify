deef22da01527ee9853c1ab7e7f8f8bc440dae45
maytag <96770043+maytag9@users.noreply.github.com>
2023-03-12 22:02:50 -0400

    update deps, remove formik

--- src/components/ThemeSwitch/index.tsx
+++ src/components/ThemeSwitch/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { BulbFilled, BulbOutlined } from '@ant-design/icons';
 import { Switch, Space } from 'antd';
 import './styles.css';
@@ -6,30 +6,29 @@ interface IThemeSwitch {
   mode: boolean;
   setMode: (mode: boolean) => void;
 }
-const ThemeSwitch: React.FunctionComponent<IThemeSwitch> = ({mode, setMode}) => {
-
-  return (<Space direction="vertical" className="theme-switch-wrapper">
-    <Switch
-      className="theme-switch"
-      checkedChildren={<BulbOutlined />}
-      unCheckedChildren={<BulbFilled />}
-      size="small"
-      onClick={() => {
+const ThemeSwitch: React.FunctionComponent<IThemeSwitch> = ({ mode, setMode }) => {
+  return (
+    <Space direction="vertical" className="theme-switch-wrapper">
+      <Switch
+        className="theme-switch"
+        checkedChildren={<BulbOutlined />}
+        unCheckedChildren={<BulbFilled />}
+        size="small"
+        onClick={() => {
           setMode(!mode);
           const nav = document.getElementById('nav-bar');
-          console.log('nav')
+          console.log('nav');
           if (mode && nav) {
-            nav?.classList.add("dark");
-            nav?.classList.remove("light")
+            nav?.classList.add('dark');
+            nav?.classList.remove('light');
           }
           if (!mode && nav) {
-            nav?.classList.add("light");
-            nav?.classList.remove("dark")
+            nav?.classList.add('light');
+            nav?.classList.remove('dark');
           }
-        }
-      }
-    />
-  </Space>
+        }}
+      />
+    </Space>
   );
 };
 
